2928db95284f5d023264e7ead1b885ca26b10cf0
vzateychuk <vzateychuk@gmail.com>
2020-03-24 13:22:23 +0300

    Added required env.CHROME_BIN when Chrome not installed on the local

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -37,7 +39,7 @@ module.exports = function (config) {
     customLaunchers: {
       MyChromeHeadless: {
         base: 'ChromeHeadless',
-        flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223']
+        flags: ['--disable-translate', '--disable-extensions']
       }
     },
     singleRun: true // Karma captures browsers, runs the tests and exits
